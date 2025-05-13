fa91564248b7890bfaed79641f9670ff28955d80
Diyar <diyarfranklin2000@gmail.com>
2025-05-13 11:40:20 +0200

    Empty classes

---
+++ src/main/java/org/scrumgame/database/models/Session.java
@@ -0,0 +1,10 @@
+package org.scrumgame.database.models;
+
+public class Session {
+    private int id;
+    private int playerId;
+    private int currentLevel;
+    private int score;
+    private int monstersEncountered;
+    private boolean gameOver;
+}
