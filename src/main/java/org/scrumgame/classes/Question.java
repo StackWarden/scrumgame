174dc68000caf7f1dcc2f3ad2a9fe7bb461c0439
joeribrinks <joeri1314@gmail.com>
2025-05-15 12:43:01 +0200

    add room to database including a random question for this room.

--- src/main/java/org/scrumgame/classes/Question.java
+++ src/main/java/org/scrumgame/classes/Question.java
@@ -1,4 +1,6 @@
 package org.scrumgame.classes;
 
-public class Question {
-}
+// public record Question is a simplified immutable data carrier class which stores immutable data
+// a record automatically generates a constructor, getters (called accessors) for each field and equals(), hashCode() and toString() methods
+// all fields are private and final
+public record Question(int id, String text, String correctAnswer){}
