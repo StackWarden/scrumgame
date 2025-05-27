ad6ec67087c40f8e22982794e8c91e3af98fcd5f
Sitjn64 <Stijnlammers2005@gmail.com>
2025-05-27 12:44:02 +0200

    starter commit

---
+++ src/main/java/org/scrumgame/classes/MultipleChoiceQuestion.java
@@ -0,0 +1,7 @@
+package org.scrumgame.classes;
+
+public class MultipleChoiceQuestion extends Question{
+    public MultipleChoiceQuestion(int id, String question, String answer, String hint) {
+        super(id, question, answer, hint);
+    }
+}
