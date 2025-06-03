2784dee95a264ad26b1ce0800b146952971500eb
joeribrinks <joeri1314@gmail.com>
2025-06-03 12:32:20 +0200

    bit fancier

--- src/main/java/org/scrumgame/assistant/AssistantCommands.java
+++ src/main/java/org/scrumgame/assistant/AssistantCommands.java
@@ -1,20 +1,19 @@
 package org.scrumgame.assistant;
 
-import org.scrumgame.assistant.actions.HintAction;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
-
-import java.util.List;
 
 @ShellComponent
 public class AssistantCommands {
 
-    @ShellMethod(key = "jarvis" , value = "Activate the assistant in this room.")
-    public void runAssistant() {
-        AssistantActivator activator = new AssistantActivator(List.of(
-                new HintAction()
-        ));
+    private final AssistantActivator activator;
 
+    public AssistantCommands(AssistantActivator activator) {
+        this.activator = activator;
+    }
+
+    @ShellMethod(key = "jarvis", value = "Activate the assistant in this room.")
+    public void runAssistant() {
         activator.run();
     }
 }
--- src/main/java/org/scrumgame/assistant/actions/HintAction.java
+++ src/main/java/org/scrumgame/assistant/actions/HintAction.java
@@ -1,10 +1,28 @@
 package org.scrumgame.assistant.actions;
 
 import org.scrumgame.assistant.AssistantAction;
+import org.scrumgame.classes.Player;
+import org.scrumgame.game.GameService;
+import org.springframework.stereotype.Component;
 
+@Component
 public class HintAction implements AssistantAction {
+
+    private final GameService gameService;
+
+    public HintAction(GameService gameService) {
+        this.gameService = gameService;
+    }
+
     @Override
     public void run() {
-        System.out.println("Hint action is running.");
+        Player a = gameService.getPlayer();
+        if (a != null) {
+            System.out.println("Hint action is running. " + a.getName());
+        }
+
+        if (a == null) {
+            System.out.println("Hint action is running, name is null.");
+        }
     }
 }
