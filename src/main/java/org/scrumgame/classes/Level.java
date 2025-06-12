ff06229490556790131a1cc9fb33667b6739119d
joeribrinks <joeri1314@gmail.com>
2025-06-12 10:59:42 +0200

    remove unused vars in level

--- src/main/java/org/scrumgame/classes/Level.java
+++ src/main/java/org/scrumgame/classes/Level.java
@@ -1,7 +1,6 @@
 package org.scrumgame.classes;
 
 public abstract class Level {
-    private int scoreRequirement = 0;
     private int logId = -1;
     private boolean completed = false;
 
@@ -15,14 +14,6 @@ public abstract class Level {
 
     public void setLogId(int logId) {
         this.logId = logId;
-    }
-
-    public int getScoreRequirement() {
-        return scoreRequirement;
-    }
-
-    public void setScoreRequirement(int scoreRequirement) {
-        this.scoreRequirement = scoreRequirement;
     }
 
     public Question getQuestion() {
